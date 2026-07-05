6f2d122b4f6dac333908a1bff7db03b75657d21b
Callemunck <34102071+callemunck@users.noreply.github.com>
2026-07-05 15:20:19 +0200

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        int res = -1;
+        while (l <= r) {
+            int m = (r + l) / 2;
+            if (nums[m] < target) {
+                l = m + 1;
+            } else if (nums[m] > target) {
+                r = m - 1; 
+            } else {
+                res = m;
+                break;
+            }
+        }
+        return res;
+    }
+}
